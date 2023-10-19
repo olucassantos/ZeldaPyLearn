9954215be4cbc51f105ac1273e44b05fe3e1aff8
Lucas Anjos <oargus.g@gmail.com>
2023-10-19 03:36:04 -0300

    Desenhando o fundo do jogo

--- code/level.py
+++ code/level.py
@@ -48,11 +48,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_height() // 2
         self.offset = pygame.math.Vector2()
 
+        # creating the floor
+        self.floor_surf = pygame.image.load('graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
+
     def custom_draw(self, player):
 
         # Pegando o offset do player
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # Drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         # Desenhando os sprites e aplicando o offset
         # for sprite in self.sprites():
