b656eaf5b27bd4123714cf9c645c39c92efeb992
Lucas Anjos <oargus.g@gmail.com>
2023-10-19 03:05:50 -0300

    Camera!

--- code/level.py
+++ code/level.py
@@ -1,4 +1,6 @@
+from typing import Iterable, Union
 import pygame
+from pygame.sprite import AbstractGroup
 from settings import *
 from tile import Tile
 from player import Player
@@ -7,10 +9,10 @@ from debug import debug
 class Level:
     def __init__(self):
         # get the display surface
-        self.display_surface = pygame.display.get_surface() # Pega todas as surfaces do código
+        # self.display_surface = pygame.display.get_surface() # Pega todas as surfaces do código
 
         # Sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
@@ -34,6 +36,25 @@ class Level:
 
     def run(self):
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_width() // 2
+        self.half_height = self.display_surface.get_height() // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+
+        # Pegando o offset do player
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        # Desenhando os sprites e aplicando o offset
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
